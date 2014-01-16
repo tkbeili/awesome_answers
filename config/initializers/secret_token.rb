42adc9a1f2cb49f298915e43e01dbd48a599941e
Tammam Kbeili <tammam.kbeili@gmail.com>
2014-01-16 15:00:53 -0800

    add question controller with placeholder index page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwesomeAnswers::Application.config.secret_key_base = '22a40d21e6afc6c9319a23cb745ea9d863385199b2a6577638f1aa0b67d34dcca761a0e7f3861121732bcb3c2e6a01a2e0cf47e39b85268c877d89b1cb35bd36'
